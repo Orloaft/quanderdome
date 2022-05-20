92454dea1777281810292f98f43399f071b11722
Orloaft <alexorlow17@gmail.com>
2022-05-20 17:46:01 -0400

    create client folder to hold nextJs app

---
+++ app/pages/Dashboard.js
@@ -0,0 +1,17 @@
+import { signOut } from "next-auth/react";
+import styles from "../styles/LoginPage.module.scss";
+import { useSession } from "next-auth/react";
+import socketService from "../services/socketService";
+
+export default function Dashboard() {
+  socketService.connect("http://localhost:9000");
+  return (
+    <div className={styles.container}>
+      <section className={styles.home}>
+        <h1 className={styles.home__heading}> QuanderDome</h1>
+        {/* <span>signed in as {user}</span> */}
+        {/* <button onClick={signOut}>Sign out</button> */}
+      </section>
+    </div>
+  );
+}
