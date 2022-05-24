b291d7b3e6d67ab0604efd3cbcf507741bcfcda3
Orloaft <alexorlow17@gmail.com>
2022-05-24 13:23:38 -0400

    add sass viariables

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fcaf66961bf0d769"; }
+/******/ 		__webpack_require__.h = function() { return "d44ec1bf89186183"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
