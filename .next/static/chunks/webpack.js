429f8d2bcdc088b90b53eca7b88e08f1788cb7da
Orloaft <alexorlow17@gmail.com>
2022-05-25 23:36:08 -0400

    add Readme FINAL BRAINSTATION COMMIT <3

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e62e9f3466bbed2"; }
+/******/ 		__webpack_require__.h = function() { return "1cf6ecc8ee0a89f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
