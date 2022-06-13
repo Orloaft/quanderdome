dee54e25873b9d225eb6720477c18e07162c682a
Alex Orlow <alexorlow17@gmail.com>
2022-06-13 13:35:10 -0400

    include cashe directories in config to speed up build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "01cc47c65351d8e6"; }
+/******/ 		__webpack_require__.h = function() { return "855b923bf7308913"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
