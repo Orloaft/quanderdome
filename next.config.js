732117f151a400c44ce2b11994cb4d95d2939b38
Alex Orlow <alexorlow17@gmail.com>
2022-06-16 16:42:09 -0400

    add typescript to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-};
 
-module.exports = nextConfig;
+module.exports = {
+  nextConfig: {
+    reactStrictMode: false,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
