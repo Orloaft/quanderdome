339ac676092adf0a1efd69f3029a7613bda0fdc1
Orloaft <alexorlow17@gmail.com>
2022-06-30 16:48:30 -0400

    comment out email sign in component for now

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -9,39 +9,39 @@ export default function LoginPage({ logIn }) {
   const handleNameChange = (e) => {
     setNameInput(e.target.value);
   }; //if authentication was succesful render login page to let user choose a name
-  if (session) {
-    return (
-      <div className={styles.container}>
-        <Particles />
-        <section className={styles.login}>
-          <h1 className={styles.login__heading}> Welcome to the QuanderDome</h1>
-          <form className={styles.sign_in_form}>
-            <input
-              onChange={(e) => {
-                handleNameChange(e);
-              }}
-              className={styles.input}
-              placeholder="username"
-            ></input>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                if (nameInput.length > 4) {
-                  window.localStorage.setItem("username", nameInput);
-                  logIn();
-                } else {
-                  alert("please use at least 5 characters");
-                }
-              }}
-              className={styles.button}
-            >
-              Sign in
-            </button>
-          </form>
-        </section>
-      </div>
-    );
-  } else {
+  // if (session) {
+  //   return (
+  //     <div className={styles.container}>
+  //       <Particles />
+  //       <section className={styles.login}>
+  //         <h1 className={styles.login__heading}> Welcome to the QuanderDome</h1>
+  //         <form className={styles.sign_in_form}>
+  //           <input
+  //             onChange={(e) => {
+  //               handleNameChange(e);
+  //             }}
+  //             className={styles.input}
+  //             placeholder="username"
+  //           ></input>
+  //           <button
+  //             onClick={(e) => {
+  //               e.preventDefault();
+  //               if (nameInput.length > 4) {
+  //                 window.localStorage.setItem("username", nameInput);
+  //                 logIn();
+  //               } else {
+  //                 alert("please use at least 5 characters");
+  //               }
+  //             }}
+  //             className={styles.button}
+  //           >
+  //             Sign in
+  //           </button>
+  //         </form>
+  //       </section>
+  //     </div>
+  //   );
+  // } else {
     //if session cat be fetched then render button for nextAuth sign in
     return (
       <div className={styles.container}>
