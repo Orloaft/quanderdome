527a9ee4858666e7003b731b86626065330233c4
Orloaft <alexorlow17@gmail.com>
2022-06-30 16:54:13 -0400

    fix typo in login component

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -10,55 +10,55 @@ export default function LoginPage({ logIn }) {
     setNameInput(e.target.value);
   }; //if authentication was succesful render login page to let user choose a name
   // if (session) {
+  return (
+    <div className={styles.container}>
+      <Particles />
+      <section className={styles.login}>
+        <h1 className={styles.login__heading}> Welcome to the QuanderDome</h1>
+        <form className={styles.sign_in_form}>
+          <input
+            onChange={(e) => {
+              handleNameChange(e);
+            }}
+            className={styles.input}
+            placeholder="username"
+          ></input>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              if (nameInput.length > 4) {
+                window.localStorage.setItem("username", nameInput);
+                logIn();
+              } else {
+                alert("please use at least 5 characters");
+              }
+            }}
+            className={styles.button}
+          >
+            Sign in
+          </button>
+        </form>
+      </section>
+    </div>
+  );
+  // } else {
+  //   //if session cat be fetched then render button for nextAuth sign in
   //   return (
   //     <div className={styles.container}>
-  //       <Particles />
   //       <section className={styles.login}>
   //         <h1 className={styles.login__heading}> Welcome to the QuanderDome</h1>
-  //         <form className={styles.sign_in_form}>
-  //           <input
-  //             onChange={(e) => {
-  //               handleNameChange(e);
-  //             }}
-  //             className={styles.input}
-  //             placeholder="username"
-  //           ></input>
-  //           <button
-  //             onClick={(e) => {
-  //               e.preventDefault();
-  //               if (nameInput.length > 4) {
-  //                 window.localStorage.setItem("username", nameInput);
-  //                 logIn();
-  //               } else {
-  //                 alert("please use at least 5 characters");
-  //               }
-  //             }}
-  //             className={styles.button}
-  //           >
-  //             Sign in
-  //           </button>
-  //         </form>
+  //         <a
+  //           href={`/api/auth/signin`}
+  //           className={styles.button}
+  //           onClick={(e) => {
+  //             e.preventDefault();
+  //             signIn();
+  //           }}
+  //         >
+  //           Sign in
+  //         </a>
   //       </section>
   //     </div>
   //   );
-  // } else {
-    //if session cat be fetched then render button for nextAuth sign in
-    return (
-      <div className={styles.container}>
-        <section className={styles.login}>
-          <h1 className={styles.login__heading}> Welcome to the QuanderDome</h1>
-          <a
-            href={`/api/auth/signin`}
-            className={styles.button}
-            onClick={(e) => {
-              e.preventDefault();
-              signIn();
-            }}
-          >
-            Sign in
-          </a>
-        </section>
-      </div>
-    );
-  }
+  // }
 }
